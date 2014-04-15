6cf724a58360b5baf69b0c9ce778c879411df466
JaeHo Song <thruthesky@gmail.com>
2014-04-15 20:00:27 +0900

    updating source codes

--- config.php
+++ config.php
@@ -1,4 +1,10 @@
 <?php
 	include widget_config_form('title');
+	include widget_config_form('forum');
+	widget_config_extra_begin();
+	include widget_config_form('width');
+	include widget_config_form('height');
 	include widget_config_form('css');
+	widget_config_extra_end();
+	
 	
--- post-with-image.php
+++ post-with-image.php
@@ -4,25 +4,36 @@
  *  @version 0.2
  */
 	widget_css();
+	
+	$bo_table = $widget_config['forum1'];
+	$height = $widget_config['height'];
+	$width = $widget_config['width'];
+	
+	if ( empty($bo_table) ) $bo_table = bo_table(1);
+	if ( empty($height) ) $height = 179;
+	if ( empty($width) ) $width = 245;
+	
+	
+	
 ?>
 <div class='post-with-image'>
-	<div class='title'><?=$widget_config['title']?></div>
+	<div class='title'><a href='<?=url_forum_list( $bo_table )?>'><?=$widget_config['title']?></a></div>
 	<div class='gallery_with_image'>
 	<?
-		$posts_with_image = g::posts(array( 'bo_table' => $widget_config['bo_table'],'wr_is_comment' => '0', 'limit' => '4', ));
-		$board_subject = db::result("SELECT bo_subject FROM $g5[board_table] WHERE bo_table='$widget_config[bo_table]'");
+		$posts_with_image = g::posts(array( 'bo_table' => $bo_table,'wr_is_comment' => '0', 'limit' => '4', ));
+		$board_subject = db::result("SELECT bo_subject FROM $g5[board_table] WHERE bo_table='$bo_table'");
 		
 		if ( $posts_with_image ) {
 			$post_number = 1;
 
 			foreach ( $posts_with_image as $post ) {
-				$post_content = db::result("SELECT wr_content FROM $g5[write_prefix]$widget_config[bo_table] WHERE wr_id='$post[wr_id]'");
-				$imgsrc = get_list_thumbnail($widget_config['bo_table'], $post['wr_id'], $widget_config['width'], $widget_config['height']);
+				$post_content = db::result("SELECT wr_content FROM $g5[write_prefix]$bo_table WHERE wr_id='$post[wr_id]'");
+				$imgsrc = get_list_thumbnail($bo_table, $post['wr_id'], $widget_config['width'], $widget_config['height']);
 				if ( $imgsrc['src'] ) {
 					$img = $imgsrc['src'];
-				} elseif ( $image_from_tag = g::thumbnail_from_image_tag( $post_content, $widget_config['bo_table'], $widget_config['width'], $widget_config['height'] )) {
+				} elseif ( $image_from_tag = g::thumbnail_from_image_tag( $post_content, $bo_table, $widget_config['width'], $widget_config['height'] )) {
 					$img = $image_from_tag;
-				} else $img = g::thumbnail_from_image_tag("<img src='".$latest_skin_url."/img/no-image.png'/>", $widget_config['bo_table'], $widget_config['width'], $widget_config['height']);
+				} else $img = g::thumbnail_from_image_tag("<img src='".$latest_skin_url."/img/no-image.png'/>", $bo_table, $widget_config['width'], $widget_config['height']);
 				?>
 				<div class='image-post post_<?=$post_number++?>'>
 					<?
@@ -49,6 +60,10 @@
 				</style>
 				<?}?>
 					
-			<?}?>
+			<?
+				} else {
+					echo "No data";
+				}
+			?>
 	</div>
 </div>
