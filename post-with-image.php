469cdba12eb1ba214214a00c17c9ce874515cfe5
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-21 17:11:33 +0800

    variable fix

--- post-with-image.php
+++ post-with-image.php
@@ -5,11 +5,11 @@
  */
 	widget_css();
 	
-	$bo_table = $widget_config['forum1'];
+	$_bo_table = $widget_config['forum1'];
 	$height = $widget_config['height'];
 	$width = $widget_config['width'];
 	
-	if ( empty($bo_table) ) $bo_table = bo_table(1);
+	if ( empty($_bo_table) ) $_bo_table = bo_table(1);
 	if ( empty($height) ) $height = 179;
 	if ( empty($width) ) $width = 245;
 	
@@ -17,27 +17,27 @@
 	
 ?>
 <div class='post-with-image'>
-	<div class='title'><a href='<?=url_forum_list( $bo_table )?>'><?=$widget_config['title']?></a></div>
+	<div class='title'><a href='<?=url_forum_list( $_bo_table )?>'><?=$widget_config['title']?></a></div>
 	<div class='gallery_with_image'>
 	<?
-		$posts_with_image = g::posts(array( 'bo_table' => $bo_table,'wr_is_comment' => '0', 'limit' => '4', ));
-		$board_subject = db::result("SELECT bo_subject FROM $g5[board_table] WHERE bo_table='$bo_table'");
+		$posts_with_image = g::posts(array( 'bo_table' => $_bo_table,'wr_is_comment' => '0', 'limit' => '4', ));
+		$board_subject = db::result("SELECT bo_subject FROM $g5[board_table] WHERE bo_table='$_bo_table'");
 		
 		if ( $posts_with_image ) {
 			$post_number = 1;
 
 			foreach ( $posts_with_image as $post ) {
-				$post_content = db::result("SELECT wr_content FROM $g5[write_prefix]$bo_table WHERE wr_id='$post[wr_id]'");
-				$imgsrc = get_list_thumbnail($bo_table, $post['wr_id'], $width, $height);
+				$post_content = db::result("SELECT wr_content FROM $g5[write_prefix]$_bo_table WHERE wr_id='$post[wr_id]'");
+				$imgsrc = get_list_thumbnail($_bo_table, $post['wr_id'], $width, $height);
 				if ( $imgsrc['src'] ) {
 					$img = $imgsrc['src'];
-				} elseif ( $image_from_tag = g::thumbnail_from_image_tag( $post_content, $bo_table, $width, $height )) {
+				} elseif ( $image_from_tag = g::thumbnail_from_image_tag( $post_content, $_bo_table, $width, $height )) {
 					$img = $image_from_tag;
-				} else $img = g::thumbnail_from_image_tag("<img src='".x::url()."/widget/$widget_config[name]/no-image.png'/>", $bo_table, $width, $height);
+				} else $img = g::thumbnail_from_image_tag("<img src='".x::url()."/widget/$widget_config[name]/no-image.png'/>", $_bo_table, $width, $height);
 				?>
 				<div class='image-post post_<?=$post_number++?>'>
 					<?
-							$url = g::url()."/bbs/board.php?bo_table=$bo_table&wr_id=$post[wr_id]";
+							$url = g::url()."/bbs/board.php?bo_table=$_bo_table&wr_id=$post[wr_id]";
 							$subject = cut_str($post['wr_subject'],15,'');
 							$content = cut_str(strip_tags($post_content), 60,'');
 					?> 
@@ -62,7 +62,7 @@
 					
 			<?
 				} else { ?>
-					<div class='title'><a href="<?=g::url()?>/bbs/write.php?bo_table=<?=$bo_table?>">NO POST CLICK HERE TO CREATE ONE</a></div>
+					<div class='title'><a href="<?=g::url()?>/bbs/write.php?bo_table=<?=$_bo_table?>">NO POST CLICK HERE TO CREATE ONE</a></div>
 					<img src="<?=x::url()?>/widget/<?=$widget_config['name']?>/no-post.jpg"/>
 					<?}?>
 	</div>
